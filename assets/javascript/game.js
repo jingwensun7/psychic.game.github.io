db45f44b5d8985e401d958d1c0a5dacb92d4d74b
Jingwen Sun <nataly.jingwen.sun@natalys-mbp.attlocal.net>
2019-04-01 20:41:48 -0700

    fix the computer guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,10 +16,9 @@ function userGuess() {
 }
 
 function game() {
-	document.querySelector("#guesses").innerHTML = "Guesses Left: 0";
-	document.querySelector("#user").innerHTML = "Your Guesses so far: 9";
-	guesses = 9;
 	user = [];
+	guesses = 9;
+	computerGuesses = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 }
 
 document.onkeyup = function(event) {
@@ -29,18 +28,18 @@ document.onkeyup = function(event) {
 	countGuesses();
 	userGuess();
 
-	var computerGuesses = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
 	if (userGuesses === computerGuesses) {
 		wins++;
 		alert("You won!");
 		document.querySelector("#wins").innerHTML = "Wins: " + wins;
+		document.querySelector("#guesses").innerHTML = "Guesses Left: 9";
 		game();
 	}
 	else if (guesses === 0) {
 		losses++;
 		alert("You lose!");
 		document.querySelector("#losses").innerHTML = "Loses: " + losses;
+		document.querySelector("#guesses").innerHTML = "Guesses Left: 9";
 		game();
 	}
 
